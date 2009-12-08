9abf57f7a1e895f8edcd4fa8a1dfa4d246330232
Leon Brocard <acme@astray.com>
2009-12-08 12:38:45 +0000

    add docs

--- lib/CPAN/Mini/Live.pm
+++ lib/CPAN/Mini/Live.pm
@@ -10,10 +10,11 @@ sub update_mirror {
     $self = $self->new(@_) unless ref $self;
 
     # first we have to catch up if we've missed anything
+    $self->trace("updating mirror...\n");
     $self->SUPER::update_mirror();
 
     # and now we try being live
-    warn "and live...\n";
+    $self->trace("and live...\n");
     my $done   = AnyEvent->condvar;
     my $client = AnyEvent::FriendFeed::Realtime->new(
         request  => '/feed/minicpan',
@@ -27,7 +28,7 @@ sub update_mirror {
             $path =~ s/^$remote//;
             my $local_file
                 = File::Spec->catfile( $self->{local}, split m{/}, $path );
-            warn "live [$action] [$path]";
+            $self->trace("live [$action] [$path]\n");
 
             if ( $action eq 'mirror_file' ) {
                 $self->mirror_file($path);
@@ -35,6 +36,7 @@ sub update_mirror {
                 $self->clean_file($local_file);
             } else {
                 warn "ERROR: unknown action $action";
+                $done->send;
             }
         },
         on_error => sub {
@@ -49,3 +51,51 @@ sub update_mirror {
 
 __END__
 
+=head1 NAME
+
+CPAN::Mini::Live - Keep CPAN Mini up to date
+
+=head1 SYNOPSIS
+
+  # have a ~/.minicpanrc:
+  # (change local to where you want the mirror)
+  remote: http://cpan.cpantesters.org/
+  exact_mirror: 1
+  force: 0
+  trace: 0
+  class: CPAN::Mini::Live
+  local: /home/acme/Public/minicpanlive/
+
+  # then run the minicpan command:
+  % minicpan
+  updating mirror...
+  and live...
+
+=head1 DESCRIPTION
+
+L<CPAN::Mini> creates a minimal mirror of CPAN and is very useful
+indeed. However, to keep the mirror up to date, you must continually
+run minicpan. This module makes minicpan block and listen for live
+updates, thus keeping your minicpan live.
+
+It works by having a backend running L<CPAN::Mini::Live::Publish>
+which publishes file updates and deletions to FriendFeed.
+
+L<CPAN::Mini::Live> listens to these updates using the FriendFeed
+real-time API so your minicpan will be kept up to date in real time.
+
+You have to use the same mirror as the backend code,
+L<http://cpan.cpantesters.org/>.
+
+=head1 SEE ALSO
+
+L<CPAN::Mini>.
+
+=head1 COPYRIGHT
+
+Copyright (C) 2009, Leon Brocard
+
+=head1 LICENSE
+
+This module is free software; you can redistribute it or modify it
+under the same terms as Perl itself.
